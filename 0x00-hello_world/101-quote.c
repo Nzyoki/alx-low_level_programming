c91dabbb2b816fcf73bb5630e3eb9f54c604d3cd
scola <scolanzioki18@gmail.com>
2023-02-16 13:50:03 -0800

    scola

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-    fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+/**
+ * main - Prints a statement to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
