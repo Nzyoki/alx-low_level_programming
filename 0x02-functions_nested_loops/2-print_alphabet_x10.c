c0054eac8ccf180972f4d984598de350d79297e1
scola <scolanzioki18@gmail.com>
2023-02-20 23:21:02 -0800

    scola

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int coun = 0;
+	char letter;
+
+	while (count++ <=9)
+	{
+		for (letter = 'a'; letter <='z'; letter++)
+			_putchar(letter);
+		_putchar('\n);
+	}
+}
