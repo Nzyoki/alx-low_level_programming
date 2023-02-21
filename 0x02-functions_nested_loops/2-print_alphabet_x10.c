4db58c01bcf967c0497e5e8d346fe5e08527d449
scola <scolanzioki18@gmail.com>
2023-02-21 05:13:52 -0800

    scola

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,12 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - prints alphabet 10 times
- *
- * Return: always 0;
- *
- */
-void print_alphabet_x10(void)
-{
-	int i;
-	char h;
 
-	for (h = 0; h <= 9; h++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
-	}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
 }
