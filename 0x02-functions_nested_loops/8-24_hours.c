3e8fea1327a99387d01a5d50f9b824ec3e3783a2
scola <scolanzioki18@gmail.com>
2023-02-22 01:58:20 -0800

    scola

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,15 @@
 {
 	int hour, minute;
 
-	for (hour = 0; minute <= 59; minute++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0');
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+		}
 	}
 }
