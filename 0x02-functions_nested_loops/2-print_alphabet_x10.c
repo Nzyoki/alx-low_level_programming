c7afc1f4748d50af600a23a0b0a55fde966a9d2d
scola <scolanzioki18@gmail.com>
2023-02-21 23:35:21 -0800

    scola

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
  *Return: always 0
  */
 void print_alphabet_x10(void)
