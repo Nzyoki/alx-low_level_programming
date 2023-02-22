94896fcb315157835fc05cadf74a02071ed14f9c
scola <scolanzioki18@gmail.com>
2023-02-22 00:26:30 -0800

    scola

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,20 +2,20 @@
 /**
  * print_sign - main function
  *
- * @c: character to compare
+ * @n: character to compare
  *
  * Return: 1 if positive, 0 if zero, -1 if negative
  *
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if (c == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
