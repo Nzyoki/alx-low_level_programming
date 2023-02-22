0d70cb0e2f6b2a4e6066aa67c779f66631ad0b1c
scola <scolanzioki18@gmail.com>
2023-02-22 00:13:21 -0800

    scola

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ *
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+i}
