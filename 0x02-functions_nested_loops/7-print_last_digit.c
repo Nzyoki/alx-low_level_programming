7ee5439d46e0b0ee0135da7fa32b6088d40ce692
scola <scolanzioki18@gmail.com>
2023-02-22 01:20:56 -0800

    scola

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
+ *
  * @n: the value
- * REturn: the value of the last digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
