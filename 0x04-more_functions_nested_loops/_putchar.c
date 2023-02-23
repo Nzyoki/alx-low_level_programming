234aa85c05cbc9d19368063ce5420a18bc35322b
scola <scolanzioki18@gmail.com>
2023-02-22 23:42:42 -0800

    scola

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * @c: the character to print
+ * Return: on success 1
+ * on error, -1 is returned, and erno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
